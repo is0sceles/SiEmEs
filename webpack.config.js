b88d5496a3ed0aa108f4bd4bf816dc2f283f8f08
Wasiff <wasiff@gmail.com>
2017-06-08 16:31:48 -0700

    add webpack and config

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const webpack = require('webpack');
+const path = require('path');
+
+// used to serve app
+const DIST_DIR = path.resolve(__dirname, 'dist');
+const SRC_DIR = path.resolve(__dirname, 'src');
+
+const config = {
+  entry: `${SRC_DIR}/app/index.js`,
+  output: {
+    path: `${DIST_DIR}/app`,
+    filename: 'bundle.js',
+    publicPath: '/app/',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js?/,
+        include: SRC_DIR,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-0'],
+        },
+      },
+    ],
+  },
+};
+
+
+module.exports = config;
