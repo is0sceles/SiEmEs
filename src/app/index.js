29db99bd6994e1a2061d7e37fa2fde92df7f62a1
Wasiff <wasiff@gmail.com>
2017-06-08 18:43:48 -0700

    render app

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,31 @@
-console.log('it works!');
+import React from 'react';
+import { render } from 'react-dom';
+
+import { Header } from './components/Header';
+import { Home } from './components/Home';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <div className="container">
+          <div className="row">
+            <div className="col-xs-10 col-xs-offset-1">
+              <h3> something goes here... </h3>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-xs-10 col-xs-offset-1">
+              <Home />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+
+render(<App />, document.querySelector('#app'))
+;
